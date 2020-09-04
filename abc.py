f80cc072f617f7737d009edbf7911b72bfe09101
SohanBOL <sohan.kathait@lji.io>
2020-09-04 21:07:03 +0530

    caching added in ci

--- abc.py
+++ abc.py
@@ -6,7 +6,7 @@ loc = ("abc.xlsx")
 wb = xlrd.open_workbook(loc) 
 sheet = wb.sheet_by_index(0) 
 sheet.cell_value(0, 0) 
-members=[]
+members=[]#sdfgdfgsd
 for i in range(sheet.nrows): 
     members.append(str(int(sheet.cell_value(i, 0))))
 # print(members)##
